cb13bdd0a29504e1476c69290ac43e8e29357f8e
Jimmy Lin <usmcjimmy@gmail.com>
2016-10-14 16:47:36 -0400

    added add profile pic

--- app/server/api/applicants.js
+++ app/server/api/applicants.js
@@ -2,9 +2,17 @@
 
 const express = require('express');
 const applicants = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'app/images/profile_images/'})
 // const bodyParser = require('body-parser');
 
 const db = require('../../db/db_apex');
+
+applicants.route('/upload')
+  .post(upload.any(), db.uploadProfileLogo, (req, res) => {
+    console.log("uploading Profile Logo:", req.files)
+    res.send(req.files);
+  })
 
 applicants.route('/')
   .get(db.showAllApplicants, (req, res) => {
@@ -13,7 +21,7 @@ applicants.route('/')
 
 applicants.route('/new')
   .post(db.postOneApplicant, (req, res) => {
-    res.status( 201 ).json( { data: 'success' } );
+    res.send(res.rows)
   });
 
 applicants.route('/:applicant_id')
