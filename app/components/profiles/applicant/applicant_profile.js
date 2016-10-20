09a10aaff6e23501a10882989e96861b1da2f153
emineKoc <eminekoc@gmail.com>
2016-10-20 18:56:12 -0400

    update the employers

--- app/components/profiles/applicant/applicant_profile.js
+++ app/components/profiles/applicant/applicant_profile.js
@@ -63,14 +63,14 @@ class Applicant_profile extends Component {
 
     console.log("certifications", certifications)
 
-
+    '/images/profile_images/'
     return(
         <div id="applicant_profile">
 
         {/* Profile Header */}
         <div className="ui grid">
           <div className="four wide column">
-            <img className="ui small circular image" src={this.state.applicantProfile.profile_image} alt="Profile Picture"/>
+            <img className="ui small circular image" src={  '/images/profile_images/' + this.state.applicantProfile.profile_image} alt="Profile Picture"/>
           </div>
           <div className="twelve wide column">
             <div className="twelve wide column">
