b76a75a50fc0bb0e239cf59816be8cf37c2b26bb
Jimmy Lin <usmcjimmy@gmail.com>
2016-10-21 11:16:33 -0400

    added files

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,34 +7,34 @@ import requireAuth from '../utils/requireAuth';
 import requireAuth_emp from '../utils/requireAuth_emp';
 
 
-
+// main
 import App from 'containers/App';
 import Main from 'containers/Main';
 
-import SignupPage from 'components/signup/SignupPage';
-import SignupPage_emp from 'components/signup/SignupPage_emp';
-import LoginPage from 'components/login/LoginPage';
-import LoginPage_emp from 'components/login/LoginPage_emp';
+// forms
+import Applicant_profile_form from 'components/forms/applicants/applicant_profile_form';
+import Employer_profile_form from 'components/forms/employers/employer_profile_form';
 
-import Applicant_profile_form from 'components/applicant/applicant_profile_form';
-import Employer_profile_form from 'components/employers/employer_profile_form';
+// profiles
 import Applicant_profile from 'components/profiles/applicant/applicant_profile';
 import Employer_profile from 'components/profiles/employer/employer_profile';
 
 
-// import Applicant_login from 'components/user_auth/applicant_login';
-// import Employer_login from 'components/user_auth/employer_login';
-// import Applicant_profile_form from 'components/forms/applicant_profile_form';
-// import Employer_profile_form from 'components/forms/employer_profile_form';
-// import Applicant_profile from 'components/profiles/applicant/applicant_profile';
-// import Employer_profile from 'components/profiles/employer/employer_profile';
+// user auth
+import SignupPage from 'components/user_auth/signup/SignupPage';
+import SignupPage_emp from 'components/user_auth/signup/SignupPage_emp';
+import LoginPage from 'components/user_auth/login/LoginPage';
+import LoginPage_emp from 'components/user_auth/login/LoginPage_emp';
+
+
+// employer jobs
 import Post_job from 'components/jobs/post_job';
 import List_jobs from 'components/jobs/list_jobs';
 import Archived_jobs from 'components/jobs/archived_jobs';
 import Job from 'components/jobs/job';
 
+// applicant / employer matching
 import List_match from 'components/applicants/list_match';
-
 import List_matched_applicants from 'components/applicants/list_matched_applicants';
 import Matched_applicant from 'components/applicants/matched_applicant_profile';
 import List_matched_employers from 'components/applicants/list_matched_employers';
