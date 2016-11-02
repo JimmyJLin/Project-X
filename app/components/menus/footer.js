cd20acc40a7cf35a92ec1196f54e452b5f147f80
Jimmy Lin <usmcjimmy@gmail.com>
2016-11-02 00:08:24 -0400

    added terms and condition page

--- app/components/menus/footer.js
+++ app/components/menus/footer.js
@@ -7,8 +7,25 @@ export default class Footer extends Component {
     return(
 
       <div id="footer">
+        <img className="ui centered rounded small image" src="images/company_logo/apex_logo.png" alt="Company Logo"/>
 
-        <p>&copy; 2016 AtlasCV.  All rights reserved</p>
+        <div className="ui equal width grid">
+          <div className="column"></div>
+          <div className="column">
+            <Link to="/#"><span>Contact Us</span></Link>
+          </div>
+          <div className="column">
+            <Link to="/terms_and_conditions#main_content"><span>Terms and Conditions</span></Link>
+          </div>
+          <div className="column">
+            <Link to="/#"><span>Privacy</span></Link>
+          </div>
+          <div className="column"></div>
+        </div>
+
+        <div id="copyrights">
+          <p>&copy; 2016 AtlasCV.  All rights reserved</p>
+        </div>
 
       </div>
 
